767c25f88857ca2825487fe57c211fcf1a17e5dc
Nick Gable <gngable@gmail.com>
2016-06-01 12:23:56 -0500

    Filled out functionality for the session activity

--- YogaTimer/app/src/main/java/com/mercangelsoftware/yogatimer/SessionsActivity.java
+++ YogaTimer/app/src/main/java/com/mercangelsoftware/yogatimer/SessionsActivity.java
@@ -31,12 +31,13 @@ import android.content.*;
 import java.util.*;
 import android.widget.*;
 import android.widget.LinearLayout.*;
+import android.widget.AdapterView.*;
 
 public class SessionsActivity extends Activity implements SingleLineDialog.SingleLineDialogListener
 {
-	
+
 	SingleLineDialog sd = new SingleLineDialog();
-	
+
 	@Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -44,11 +45,18 @@ public class SessionsActivity extends Activity implements SingleLineDialog.Singl
         setContentView(R.layout.activity_sessions);
 		loadSessions();
     }
-	
-	public void addSession(View view){
+
+	public void addSession(View view)
+	{
 		sd.setTitle("Please Enter Title");
 		sd.setPositiveText("Add");
 		sd.show(getFragmentManager(), "SessionNameDialog");
+	}
+
+	public void clearSessions(View view)
+	{
+		saveSessions(new HashSet<String>());
+		loadSessions();
 	}
 
 	@Override
@@ -57,38 +65,122 @@ public class SessionsActivity extends Activity implements SingleLineDialog.Singl
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setMessage(sd.getText());
 		builder.create().show();
-		
-		
+
+		Set<String> sessions = getSessions();
+
+		HashSet<String> hs = new HashSet<>(sessions);
+
+		hs.add(sd.getText());
+
+		saveSessions(hs);
+
+		loadSessions();
 	}
 	
-	private Set<String> getSessions(){
-		SharedPreferences preferences = getApplicationContext().getSharedPreferences(GlobalsAndStatics.PREFNAME, Context.MODE_PRIVATE);
-		return preferences.getStringSet("sessions", null);
-	}
-	
-	private void loadSessions(){
+	public void deleteSession(String session){
 		Set<String> sessions = getSessions();
 		
-		ListView lv = (ListView)findViewById(R.id.sessionslist);
+		HashSet<String> hs = new HashSet<>();
 		
-		lv.removeAllViewsInLayout();
-		
-		if (sessions == null) return;
-		
-		for (String session : sessions){
-			lv.addView(getSessionView(session));
+		for(String s : sessions){
+			if (!s.equals(session)){
+				hs.add(s);
+			}
 		}
+		
+		saveSessions(hs);
+
+		loadSessions();
+	}
+
+	private Set<String> getSessions()
+	{
+		SharedPreferences preferences = getApplicationContext().getSharedPreferences(GlobalsAndStatics.PREFNAME, Context.MODE_PRIVATE);
+		return preferences.getStringSet("sessions", new HashSet<String>());
+	}
+
+	private void saveSessions(Set<String> sessions)
+	{
+		SharedPreferences preferences = getApplicationContext().getSharedPreferences(GlobalsAndStatics.PREFNAME, Context.MODE_PRIVATE);
+		SharedPreferences.Editor ed = preferences.edit();
+
+		ed.putStringSet("sessions", sessions);
+
+		ed.commit();
+	}
+
+	private void loadSessions()
+	{
+		Set<String> sessions = getSessions();
+
+		ListView lv = (ListView)findViewById(R.id.sessionslist);
+
+		lv.removeAllViewsInLayout();
+
+		//InfoPop("" + sessions.size());
+
+		if (sessions.isEmpty()) return;
+
+		//String[] arr = new String[sessions.size()];
+
+		//for (String session : sessions){
+		//lv.addView(getSessionView(session));
+
+		//}
+
+		ArrayAdapter<String> modeAdapter = new ArrayAdapter<String>(this, R.layout.list_item_session, R.id.list_item_sessionLabel, sessions.toArray(new String[sessions.size()]));
+		lv.setAdapter(modeAdapter);
+//		lv.setOnItemClickListener(new OnItemClickListener(){
+//
+//				@Override
+//				public void onItemClick(AdapterView<?> p1, View p2, int p3, long p4)
+//				{
+//					InfoPop("int = " + p3 + " long = " + p4);
+//				}
+//
+//				
+//			});
+	}
+
+	public void InfoPop(String text)
+	{
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(text);
+		builder.create().show();
 	}
 
 	private View getSessionView(String session)
 	{
 		TextView tv = new TextView(getApplicationContext());
-		
+
 		tv.setText(session);
 		tv.setLayoutParams(new LayoutParams(
-							   LinearLayout.LayoutParams.MATCH_PARENT,
-							   LinearLayout.LayoutParams.MATCH_PARENT));
-		
+							   LinearLayout.LayoutParams.WRAP_CONTENT,
+							   LinearLayout.LayoutParams.WRAP_CONTENT));
+
 		return tv;
+	}
+	
+	public void deleteClick(View view){
+		///InfoPop("delete");
+		ViewGroup vg = (ViewGroup)view.getParent();
+		
+		for (int i = 0; i < vg.getChildCount(); i++){
+			if (vg.getChildAt(i) instanceof TextView && !(vg.getChildAt(i) instanceof Button)){
+				InfoPop(((TextView)vg.getChildAt(i)).getText().toString());
+				deleteSession(((TextView)vg.getChildAt(i)).getText().toString());
+			}
+		}
+	}
+	
+	public void editClick(View view){
+		///InfoPop("delete");
+		ViewGroup vg = (ViewGroup)view.getParent();
+
+		for (int i = 0; i < vg.getChildCount(); i++){
+			if (vg.getChildAt(i) instanceof TextView && !(vg.getChildAt(i) instanceof Button)){
+				InfoPop(((TextView)vg.getChildAt(i)).getText().toString());
+			}
+		}
 	}
 }
